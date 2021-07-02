2121fac41c1b7d17b1acbe68b86ba9175f1857ad
nilavan <nila@gmail.com>
2021-07-02 11:36:30 +0200

    test

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -9,17 +9,7 @@
         <div class="col bg-success px-6 py-8 rounded-2 me-7 mb-7">
             <!--begin::Svg Icon | path: icons/stockholm/Media/Equalizer.svg-->
             <h1>Notification</h1>
-            <span class="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <rect x="0" y="0" width="24" height="24"></rect>
-                        <rect fill="#000000" opacity="0.3" x="13" y="4" width="3" height="16" rx="1.5"></rect>
-                        <rect fill="#000000" x="8" y="9" width="3" height="11" rx="1.5"></rect>
-                        <rect fill="#000000" x="18" y="11" width="3" height="9" rx="1.5"></rect>
-                        <rect fill="#000000" x="3" y="13" width="3" height="7" rx="1.5"></rect>
-                    </g>
-                </svg>
-            </span>
+            
             @foreach ($notifications as $item)
             <!--end::Svg Icon-->
             <h4 class="text-warning fw-bold fs-6">{{$item->text}}</h4>
