a4c13751d0d8795ab85264d9e87059e87ce154d8
nilavan <nila@gmail.com>
2021-07-02 11:35:25 +0200

    test

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -6,8 +6,9 @@
     </x-slot>
 <div class="row">
     <div class="col-sm-12">
-        <div class="col bg-light-warning px-6 py-8 rounded-2 me-7 mb-7">
+        <div class="col bg-success px-6 py-8 rounded-2 me-7 mb-7">
             <!--begin::Svg Icon | path: icons/stockholm/Media/Equalizer.svg-->
+            <h1>Notification</h1>
             <span class="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
